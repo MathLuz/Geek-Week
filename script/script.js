7c4dbd1723c9e08ecfad2e9a870c58c384d6dedf
MathLuz <111208300+MathLuz@users.noreply.github.com>
2023-08-07 18:36:02 -0300

    ajustes script

--- script/script.js
+++ script/script.js
@@ -2,3 +2,22 @@
 function goBack() {
     window.history.back();
 }
+// Filtros de datas
+document.addEventListener("DOMContentLoaded", function () {
+    const tagButtons = document.querySelectorAll(".tag-btn");
+    const items = document.querySelectorAll(".itens");
+
+    tagButtons.forEach(button => {
+        button.addEventListener("click", () => {
+            const selectedTag = button.getAttribute("data-tag");
+
+            items.forEach(item => {
+                if (selectedTag === "all" || item.getAttribute("data-tags").includes(selectedTag)) {
+                    item.classList.add("show");
+                } else {
+                    item.classList.remove("show");
+                }
+            });
+        });
+    });
+});
