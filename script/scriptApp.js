1a28a81dd774d5ed0c415612746924aa3bedffe0
mathluz <matheuzfluz@hotmail.com>
2023-08-11 21:34:54 -0300

    teste instal with modal

--- script/scriptApp.js
+++ script/scriptApp.js
@@ -30,11 +30,11 @@ let deferredPrompt;
 */
 
 // VENDO SE FOI ABERTO EM NO SITE
-   
-  if (!window.matchMedia('(display-mode: standalone)').matches) {
-      // Código a ser executado quando NÃO estiver em modo autônomo (navegador comum)
-      // console.log("Não está em modo autônomo");
-   
+
+if (!window.matchMedia('(display-mode: standalone)').matches) {
+  // Código a ser executado quando NÃO estiver em modo autônomo (navegador comum)
+  // console.log("Não está em modo autônomo");
+
   // Sweet Alert
   Swal.fire({
     title: 'Baixe o App!!',
@@ -48,29 +48,47 @@ let deferredPrompt;
     confirmButtonText: 'Quero baixar!'
   }).then((result) => {
     if (result.isConfirmed) {
-      Swal.fire({
-        background: '#0a0a0a',
-        color: '#fff',
-        icon: 'success',
-        title: 'Divirta-se nesta incrível semana',
-        showConfirmButton: false,
-        timer: 2000
-      })
-      
-  let deferredPrompt;
-
-  window.addEventListener('beforeinstallprompt', (event) => {
-    event.preventDefault(); // Impede o prompt automático
-    deferredPrompt = event; // Armazena o evento para uso posterior
+      baixar()
+    }
   });
-    if (deferredPrompt) {
-      // Exibe o prompt de instalação
-      deferredPrompt.prompt();
+
+  function handleDownload() {
+    let deferredPrompt;
+
+    window.addEventListener('beforeinstallprompt', (event) => {
+      event.preventDefault(); // Impede o prompt automático
+      deferredPrompt = event; // Armazena o evento para uso posterior
+    });
+
+    function exibirPrompt() {
+      if (deferredPrompt) {
+        // Exibe o prompt de instalação
+        deferredPrompt.prompt();
+
+        // Captura o resultado da escolha do usuário
+        deferredPrompt.userChoice.then((choiceResult) => {
+          if (choiceResult.outcome === 'accepted') {
+            console.log('Usuário aceitou instalar o aplicativo');
+          } else {
+            alert('Usuário recusou a instalação do aplicativo');
+          }
+
+          deferredPrompt = null; // Limpa a referência ao evento
+        });
+      }
     }
-  
-    }
-  })
-  } // else {
+
+    Swal.fire({
+      background: '#0a0a0a',
+      color: '#fff',
+      icon: 'success',
+      title: 'Divirta-se nesta incrível semana',
+      showConfirmButton: false,
+      timer: 2000
+    });
+  }
+
+} // else {
       // Código a ser executado quando estiver em modo autônomo (PWA)
       // console.log("Está em modo autônomo");
   // }
