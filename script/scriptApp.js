f6ada5049cd4ff178e4661726a7d803df75ab92a
MathLuz <111208300+MathLuz@users.noreply.github.com>
2023-08-12 14:30:19 -0300

    Update scriptApp para iOS

--- script/scriptApp.js
+++ script/scriptApp.js
@@ -15,7 +15,7 @@ if (!window.matchMedia('(display-mode: standalone)').matches) {
     // Obter a data atual
     var now = new Date();
     // Definir a data de expiração para 10 minutos após a data atual
-    var expirationDate = new Date(now.getTime() + 1 * 60 * 1000);  // min * seg * mil
+    var expirationDate = new Date(now.getTime() + 1 * 5 * 1000);  // min * seg * mil
     // Formatar a data de expiração para o formato GMT
     var expiresGMT = expirationDate.toUTCString();
     // Definir o cookie com a data de expiração ajustada
@@ -24,62 +24,63 @@ if (!window.matchMedia('(display-mode: standalone)').matches) {
   // Exibir a função apenas na primeira (dentro da quantidade de tempo) vez que o usuário acessar
   if (!hasUserVisitedPage()) {
 
-    // Sweet Alert
-    Swal.fire({
-      title: 'Baixe o App!!',
-      text: "Para ficar por dentro de tudo que precisa saber sobre a semana Geek",
-      icon: 'info',
-      background: '#0a0a0a',
-      color: '#fff',
-      showCancelButton: true,
-      confirmButtonColor: '#f4a917',
-      cancelButtonColor: '#d33',
-      confirmButtonText: 'Quero baixar!',
-      preConfirm: () => {
-        baixarApp();
-      }
-    });
-
-    // PARA BAIXAR O APP NO ANDROID
-
-    let deferredPrompt;
-
-    window.addEventListener('beforeinstallprompt', (event) => {
-      event.preventDefault(); // Impede o prompt automático
-      deferredPrompt = event; // Armazena o evento para uso posterior
-    });
-
-    function baixarApp() {
-      if (deferredPrompt) {
-
-        // Exibe o prompt de instalação
-        deferredPrompt.prompt();
-        /*
-              // Captura o resultado da escolha do usuário
-              deferredPrompt.userChoice.then((choiceResult) => {
-                if (choiceResult.outcome === 'accepted') {
-                  console.log('Usuário aceitou instalar o aplicativo');
-                }
-                deferredPrompt = null; // Limpa a referência ao evento
-              });
-        */
-      }
-    }
-
     // CASO SEJA ABERTO NO SAFARI
-    if (navigator.userAgent.indexOf("Safari") !== -1 && navigator.userAgent.indexOf("Chrome") === -1) {
+    if (/^((?!chrome|android).)*safari/i.test(navigator.userAgent)) {
+  
       //console.log("Você está usando o Safari.");
       // Sweet Alert para manual de Download no Safari
       Swal.fire({
         background: '#0a0a0a',
         color: '#fff',
         title: 'Baixe o App!!',
-        text: 'Para ficar por dentro de tudo que precisa saber sobre a semana Geek',
+        text: 'Para ficar por dentro de tudo que precisa saber sobre a semana Geek.',
         imageUrl: 'img/downloadSafari.jpg',
         imageWidth: 400,
         imageAlt: 'Baixar o App Geek Week',
         confirmButtonColor: '#f4a917',
       });
+    } else {
+      // Sweet Alert
+      Swal.fire({
+        title: 'Baixe o App!!',
+        text: "Para ficar por dentro de tudo que precisa saber sobre a semana Geek.",
+        icon: 'info',
+        background: '#0a0a0a',
+        color: '#fff',
+        showCancelButton: true,
+        confirmButtonColor: '#f4a917',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Quero baixar!',
+        preConfirm: () => {
+          baixarApp();
+        }
+      });
+  
+      // PARA BAIXAR O APP NO ANDROID
+  
+      let deferredPrompt;
+  
+      window.addEventListener('beforeinstallprompt', (event) => {
+        event.preventDefault(); // Impede o prompt automático
+        deferredPrompt = event; // Armazena o evento para uso posterior
+      });
+  
+      function baixarApp() {
+        if (deferredPrompt) {
+  
+          // Exibe o prompt de instalação
+          deferredPrompt.prompt();
+          /*
+                // Captura o resultado da escolha do usuário
+                deferredPrompt.userChoice.then((choiceResult) => {
+                  if (choiceResult.outcome === 'accepted') {
+                    console.log('Usuário aceitou instalar o aplicativo');
+                  }
+                  deferredPrompt = null; // Limpa a referência ao evento
+                });
+          */
+        }
+      }
     }
 
     // Depois de exibir a função, marque o usuário como visitado 
